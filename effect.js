21d756ffbaf88f95f97eb4d74e932920ee6b3175
Salvador Sotelo <salvadorsotelo@gmail.com>
2013-01-09 20:08:08 -0700

    Added a better way to toggle artists iframe on index.html

--- effect.js
+++ effect.js
@@ -1,28 +1,8 @@
 $(document).ready(function() {
         $('body').fadeIn(1500);
         $('#logo').addClass('animated flipInX');
-        var loadLink = document.getElementById("loadFrame");
-        loadLink.onclick = loadFrame;
-
-        function loadFrame(e){
-          var container = document.getElementById("frameHolder");
-          e = e || window.event;
-  
-          $('.arrow_box').css('display', 'inherit');
-          container.innerHTML = "<iframe src=" + loadLink.href + ">";
-          stopEvent(e);
-        }
-
-        function stopEvent(e){
-          e = e || window.event;
-  
-          e.cancelBubble = true;
-          e.returnValue = false;
-  
-          if(e.stopPropagation){
-            e.stopPropagation();
-            e.preventDefault();
-          }
-          return false;
-        }
+        $('#loadFrame').click(function(e){
+          e.preventDefault();
+          $('.arrow_box').toggle();
+        })
 });

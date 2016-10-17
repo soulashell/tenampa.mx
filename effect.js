fff2348f6933348c85dd14f1c5c4d8912b079555
soulashell <salvadorsotelo@gmail.com>
2016-10-17 13:05:46 -0600

    Deleted unused files, new logo and responsive changes

--- effect.js
+++ effect.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $('body').fadeIn(1500);
-  $('#logo').addClass('animated flipInX');
+  $('#logo').addClass('animated fadeInDown');
   $('#loadArtists').click(function(e){
     e.preventDefault();
     $('.arrow_box').animate({height: "toggle"}, 1500);
